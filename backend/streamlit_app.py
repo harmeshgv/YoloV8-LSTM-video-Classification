273dc2a8e7dd20b7474972915c2def690f7ff7ad
harmeshgv <harmeshgopinathan@gmail.com>
2025-09-16 02:03:05 +0530

    add: packages.txt

--- backend/streamlit_app.py
+++ backend/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from backend.services.video_processing.processor import VideoProcessor
 from backend.services.prediction.predictor import ViolencePredictor
