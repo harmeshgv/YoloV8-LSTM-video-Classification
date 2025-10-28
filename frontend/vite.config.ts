4113e414f8bdffac7eb9bb32e4266470ed5069d9
harmeshgv <harmeshgopinathan@gmail.com>
2025-10-28 23:41:46 +0530

    fix frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/YoloV8-LSTM-video-Classification/', // <-- your repo name
   plugins: [react()],
-  server: {
-    port: 5173, // optional, set your dev server port
-  },
 })
+
